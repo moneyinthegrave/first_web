6698dd5cca1d7aaf885f928269e7feeda4fa00ae
self care <49820562+ICANSTILLHEARYOU@users.noreply.github.com>
2021-12-06 00:48:44 +0300

    header + intro + container added

--- index.php
+++ index.php
@@ -8,10 +8,27 @@
     <link href="tema.css" rel="stylesheet">
     <title>My web browser</title>
 </head>
+
+<header class="header">
+    <div class="container">
+        <div class="header__inner">
+            <div class="logo">Mogo</div>
+            <!-- навигация -->
+            <nav>
+                <a href="">About</a>
+                <a href="">Service</a>
+                <a href="">Blog</a>
+                <a href="">Work</a>
+                <a href="">Contact </a>
+            </nav>
+        </div>
+    </div>
+</header>
 <body>
-    <?php
-     echo '<p>' . "Welcome to Mogo " . '</p>';
-    echo '<body>' . "about us ". '</body>';
-    ?>
-    </body>
+    <div class="intro">
+        <div class="container">
+
+        </div>
+    </div>
+</body>
 </html>
